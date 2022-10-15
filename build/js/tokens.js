b01d4a3611b1a1b26ffd2564fc28d4edb408df69
Neolivan <neolivan.eng@outlook.com>
2022-10-15 20:45:09 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 15 Oct 2022 23:34:32 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGrey100": "#c1c1c4",
+  "ColorGrey500": "#3f3d3d",
+  "ColorGrey800": "#1c1c1e",
+  "ColorGrey900": "#121214",
+  "ColorPurple400": "#8257e6"
+};

f31621647ab095cfcce28322e1532fd71c0c2a2a
Neolivan <Neolivan@users.noreply.github.com>
2022-10-16 00:09:47 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,28 @@
 /**
  * Do not edit directly
- * Generated on Sat, 15 Oct 2022 23:34:32 GMT
+ * Generated on Sun, 16 Oct 2022 00:09:46 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGrey100": "#c1c1c4",
   "ColorGrey500": "#3f3d3d",
   "ColorGrey800": "#1c1c1e",
   "ColorGrey900": "#121214",
-  "ColorPurple400": "#8257e6"
+  "ColorPurple400": "#8257e6",
+  "FontLabel": {"fontSize":16,"textDecoration":"none","fontFamily":"Inter","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0,"lineHeight":19.2,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
+  "TypographyLabelFontSize": 16,
+  "TypographyLabelTextDecoration": "none",
+  "TypographyLabelFontFamily": "Inter",
+  "TypographyLabelFontWeight": 400,
+  "TypographyLabelFontStyle": "normal",
+  "TypographyLabelFontStretch": "normal",
+  "TypographyLabelLetterSpacing": 0,
+  "TypographyLabelLineHeight": 19.2,
+  "TypographyLabelParagraphIndent": 0,
+  "TypographyLabelParagraphSpacing": 0,
+  "TypographyLabelTextCase": "none"
 };
